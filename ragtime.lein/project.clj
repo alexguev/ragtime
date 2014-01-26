f189a08e6888783ff0bb141d6d63cf569dc79fa6
Alexei Guevara <alexguev@DellaLu.local>
2014-01-26 17:57:02 -0500

    prepare to push patched version to clojars

--- ragtime.lein/project.clj
+++ ragtime.lein/project.clj
@@ -1,4 +1,4 @@
-(defproject ragtime/ragtime.lein "0.3.4"
+(defproject org.clojars.alexguev.ragtime/ragtime.lein "0.3.4.1"
   :description "A leiningen plugin for Ragtime."
   :dependencies [[leinjacker "0.4.1"]]
   :eval-in-leiningen true)
